8a8545f6afe862662bd0b62a91fc49ba48243098
Baraker42 <70022095+Baraker42@users.noreply.github.com>
2021-07-22 13:06:33 +0200

    Add files via upload

---
+++ ads.py
@@ -0,0 +1,32 @@
+#otevře náš soubor s našimi ads.txt
+with open("nase.txt") as f:
+    lines=f.readlines()
+
+#uloží do nově vytvořeného listu
+firstads=[]
+for i in lines:
+    if "#" not in i:
+        firstads.append(i)
+
+#otevře soubor ads.txt k porovnání a uloží do nového listu
+with open("klient.txt") as s:
+    lines2=s.readlines()
+
+secondads=[]
+for i in lines2:
+    if "#" not in i:
+        secondads.append(i)
+
+#porovná ads.txt ze stránky a vytvoří nový soubor s ads, které na stránce chybí
+counter=0
+newads=[]
+for i in firstads:
+    if firstads[counter] not in secondads:
+        newads.append(firstads[counter])
+        file= open("ads.txt", "a")
+        file.write(firstads[counter])
+    counter +=1
+
+file.close()
+
+
