d259f7c9007d39b0ec32898bf54b1fddc8b3fd15
Baraker42 <70022095+Baraker42@users.noreply.github.com>
2021-07-23 09:39:00 +0200

    optimalizace

--- ads.py
+++ ads.py
@@ -1,48 +1,41 @@
 import os
-#otevře náš soubor s našimi ads.txt
-with open("nase.txt") as f:
-    lines=f.readlines()
 
-#uloží do nově vytvořeného listu
-firstads=[]
-for i in lines:
-    if "#" not in i:
-        firstads.append(i)
+#funkce pro přepsání textového dokumentu do listu
+def transcript(txt):
+    with open(txt) as f:
+        lines=f.readlines()
+    newlist=[]
+    for i in lines:
+        if "#" not in i:
+            newlist.append(i)
+    return newlist
 
-#otevře soubor ads.txt k porovnání a uloží do nového listu
-with open("klient.txt") as s:
-    lines2=s.readlines()
+#porovná ads.txt ze stránky s našimi a vytvoří nový soubor s ads, které na stránce chybí
+def check_ads():
+    newads=[]
+    for i in firstads:
+        if i not in secondads:
+            newads.append(i)
+            file= open("ads.txt", "a")
+            file.write(i)
 
-secondads=[]
-for i in lines2:
-    if "#" not in i:
-        secondads.append(i)
+    file.close()
 
-#porovná ads.txt ze stránky a vytvoří nový soubor s ads, které na stránce chybí
+#vytvoří list s našemi ads.txt
+firstads=transcript("nase.txt")
+#vytvoří lsit s klientskými ads.txt
+secondads=transcript("klient.txt")
+
+
+
+#zkontroluje jestli existuje ads.txt soubor, pokud ano vymaže ho a vytvoří nový
 try:
     with open("ads.txt") as f:
         lines=f.readlines()
     os.remove("ads.txt")
-    counter=0
-    newads=[]
-    for i in firstads:
-        if firstads[counter] not in secondads:
-            newads.append(firstads[counter])
-            file= open("ads.txt", "a")
-            file.write(firstads[counter])
-        counter +=1
+    check_ads()
 
-    file.close()
 except FileNotFoundError:
-    counter=0
-    newads=[]
-    for i in firstads:
-        if firstads[counter] not in secondads:
-            newads.append(firstads[counter])
-            file= open("ads.txt", "a")
-            file.write(firstads[counter])
-        counter +=1
-
-    file.close()
+    check_ads()
 
 
